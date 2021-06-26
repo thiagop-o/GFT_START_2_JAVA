571f27f8bccec4c011a6c912a1fdc41c77502e63
Thiago Oliveira <devfs.thiago.oliveira@gmail.com>
2021-06-26 03:25:39 -0300

    Improved person creation functionality

---
+++ 11_projeto_gerenciamentoDePessoas_rest_springBoot/personapi/src/main/java/com/digitalinnovationone/personapi/controller/PersonController.java
@@ -0,0 +1,29 @@
+package com.digitalinnovationone.personapi.controller;
+
+import com.digitalinnovationone.personapi.dto.request.PersonDTO;
+import com.digitalinnovationone.personapi.dto.response.MessageResponseDTO;
+import com.digitalinnovationone.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+
+    private PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
+    }
+}
---
+++ 11_projeto_gerenciamentoDePessoas_rest_springBoot/personapi/src/main/java/com/digitalinnovationone/personapi/service/PersonService.java
@@ -0,0 +1,35 @@
+package com.digitalinnovationone.personapi.service;
+
+import com.digitalinnovationone.personapi.dto.request.PersonDTO;
+import com.digitalinnovationone.personapi.dto.response.MessageResponseDTO;
+import com.digitalinnovationone.personapi.entity.Person;
+import com.digitalinnovationone.personapi.mapper.PersonMapper;
+import com.digitalinnovationone.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
+        Person personToSave = personMapper.toModel(personDTO);
+
+
+        Person savedPerson = personRepository.save(personToSave);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " +savedPerson.getId())
+                .build();
+    }
+
+
+}
